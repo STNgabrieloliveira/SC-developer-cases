b19b97f735da8ca5c23b99646f9c14aff823a5a2
Lucas Alencar <alencarlucaas@gmail.com>
2020-02-19 20:22:25 -0300

    feat(part-one): Update problem 2

--- src/problema1/index.js
+++ src/problema1/index.js
@@ -2,7 +2,6 @@ const solution = (arr) => {
     // Erase the lines below and write your own solution
     const answer = {
         media: 3.14,
-        moda: 1,
         mediana: 3,
     };
     return answer;
